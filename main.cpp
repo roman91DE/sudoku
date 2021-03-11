98adaab74233da7d422e82e8982182740f0fb6a2
rohoehn <rohoehn123@gmail.com>
2021-03-11 19:33:28 +0100

    ...

--- main.cpp
+++ main.cpp
@@ -2,7 +2,11 @@
 #include <iostream>
 
 int main() {
-    Sudoku s("txt_files/grid.txt");
-    s.solve();
+    // Sudoku s("txt_files/grid.txt");
+    // s.solve(true);
+    Sudoku s;
+    s.solve_once();
+    s.print_grid_stdio();
+
     return 0;
 }
--- sudoku.h
+++ sudoku.h
@@ -15,10 +15,14 @@ class Sudoku{
         void random_filling(int n_numbers);
         void random_deletion(int n_numbers);
         void solve(bool print=true);
+        void solve_once();
         void print_grid_stdio() const;
         void set_value(int y_val, int x_val, int number);
         void clear_value(int y_val, int x_val);
         bool is_possible(int y_val, int x_val, int number) const;
         bool is_solved() const;
 };
+
+void generateSudoku(std::string filename);
+
 
